cbd2d603c2a7cbd2e65d000fb4f4c1f70ef52b1a
Kennedy Soares <kennedybarros413@gmail.com>
2022-01-25 11:12:03 -0400

    add home

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
+<h1 align="center">
+    <img src="./img/assets/img/logo.svg">
+</h1>
 
+<h1 align="center">
+    <img src="./banner.png">
+</h1>
+
+<h3 align="center">Você pode acessar o projeto <a href="" target="_blank">Clicando Aqui</a></h3>
+
+---
+
+</br>
+
+# Tecnologias
+
+- [Html](https://www.w3schools.com/html/)
+- [Css](https://www.w3schools.com/css/)
+- [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+- [GSap](https://greensock.com/gsap/)
+
+---
+
+<br/>
+
+## Como baixar o projeto:
+
+```bash
+# Clonar o repositório
+$ git clone git@github.com:kennedybarros/iphone-responsive-landing-page.git
+
+# Entrar no diretório
+$ cd iphone-responsive-landing-page
+```
+
+---
+
+<p align="center"> Desenvolvido por <a href="https://www.linkedin.com/in/kennedybarros/">Kennedy Barros</a> ✌🏼</p>
--- assets/js/main.js
+++ assets/js/main.js
@@ -11,12 +11,43 @@ const navMenu = document.getElementById('nav-menu'),
   toggleMenu = document.getElementById('nav-toggle'),
   closeMenu = document.getElementById('nav-close');
 
-// SHOW
 toggleMenu.addEventListener('click', () => {
   navMenu.classList.toggle('show');
 });
 
-// HIDDEN
 closeMenu.addEventListener('click', () => {
   navMenu.classList.remove('show');
 });
+
+document.addEventListener('mousemove', move);
+function move(e) {
+  this.querySelectorAll('.move').forEach((layer) => {
+    const speed = layer.getAttribute('data-speed');
+
+    const x = (window.innerWidth - e.pageX * speed) / 120;
+    const y = (window.innerHeight - e.pageY * speed) / 120;
+
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+}
+
+gsap.from('.nav_logo, .nav_toggle', {
+  opacity: 0,
+  duration: 1,
+  delay: 2,
+  y: 10,
+});
+gsap.from('.nav_item', {
+  opacity: 0,
+  duration: 1,
+  delay: 2.1,
+  y: 30,
+  stagger: 0.2,
+});
+
+gsap.from('.home_title', { opacity: 0, duration: 1, delay: 1.6, y: 30 });
+gsap.from('.home_description', { opacity: 0, duration: 1, delay: 1.8, y: 30 });
+gsap.from('.home_text', { opacity: 0, duration: 1, delay: 1.8, y: 30 });
+gsap.from('.home_buy', { opacity: 0, duration: 1, delay: 2.1, y: 30 });
+gsap.from('.home_learn', { opacity: 0, duration: 1, delay: 2.1, y: 30 });
+gsap.from('.home_img', { opacity: 0, duration: 1, delay: 1.3, y: 30 });
