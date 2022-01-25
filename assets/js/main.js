860747eb3e9e62aec16c10ff488b3f8d48405782
Kennedy Barros <89874181+kennedybarros@users.noreply.github.com>
2022-01-24 23:05:58 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,12 +11,10 @@ const navMenu = document.getElementById('nav-menu'),
   toggleMenu = document.getElementById('nav-toggle'),
   closeMenu = document.getElementById('nav-close');
 
-// SHOW
 toggleMenu.addEventListener('click', () => {
   navMenu.classList.toggle('show');
 });
 
-// HIDDEN
 closeMenu.addEventListener('click', () => {
   navMenu.classList.remove('show');
 });
